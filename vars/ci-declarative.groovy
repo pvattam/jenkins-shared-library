47b0522acc4363076bc714c334abb16d5516fa4a
Pushpak Vattam <vattampushpak243@gmail.com>
2024-01-02 08:08:57 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

---
+++ vars/ci-declarative.groovy
@@ -0,0 +1,41 @@
+def call(){
+    pipeline {
+        agent{
+            node {label 'workstation'}
+        }
+
+        stages{
+
+            stage ('Compile'){
+                steps {
+                    echo 'Compile'
+                }
+            }
+
+            stage ('Test Cases'){
+                steps {
+                    echo 'Test Cases'
+                }
+            }
+
+            stage ('Integration Test Cases'){
+                steps {
+                    echo 'Integration Test Cases'
+                }
+            }
+
+            stage ('Build'){
+                steps {
+                    echo 'Build'
+                }
+            }
+
+            stage ('Release Apply'){
+                steps {
+                    echo 'Release Apply'
+                }
+            }
+
+        }
+    }
+}
--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,41 +1,10 @@
 def call(){
-    pipeline {
-        agent{
-            node {label 'workstation'}
-        }
-
-        stages{
-
-            stage ('Compile'){
-                steps {
-                    echo 'Compile'
-                }
-            }
-
-            stage ('Test Cases'){
-                steps {
-                    echo 'Test Cases'
-                }
-            }
-
-            stage ('Integration Test Cases'){
-                steps {
-                    echo 'Integration Test Cases'
-                }
-            }
-
-            stage ('Build'){
-                steps {
-                    echo 'Build'
-                }
-            }
-
-            stage ('Release Apply'){
-                steps {
-                    echo 'Release Apply'
-                }
-            }
-
-        }
+    node ('workstation'){
+        sh 'env'
+        stage('Compile'){}
+        stage('Test Cases'){}
+        stage('Integration Test Cases'){}
+        stage('Build'){}
+        stage('Release App'){}
     }
 }
