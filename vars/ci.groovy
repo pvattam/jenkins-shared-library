17097383ba72a4d40ca742dcc922386719a986e9
Pushpak Vattam <vattampushpak243@gmail.com>
2024-01-02 09:00:59 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,6 +1,20 @@
 def call(){
     node ('workstation'){
-        stage('Code Checkout'){}
+
+        sh "find . | sed -e '1d' |xargs rm -rf"
+
+        if(env.TAG_NAME ==~ ".*") {
+            env.branch_name = "refs/tags/${env.TAG_NAME}"
+        }else {
+            env.branch_name = "${env.BRANCH_NAME}"
+        }
+        stage('Code Checkout'){
+            checkout scmGit(
+                    branches: [[name: "${branch_name}"]],
+                    userRemoteConfigs: [[url: "https://github.com/pvattam/expense-backend.git"]]
+            )
+        }
+
         stage('Compile'){}
 
         if(env.BRANCH_NAME == 'main'){
