4b3aed8239cd604a3ef836fc1f6b1d6437b9a9d8
Pushpak Vattam <vattampushpak243@gmail.com>
2024-01-02 22:12:30 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -19,10 +19,10 @@ def call(){
 
         if(env.BRANCH_NAME == 'main'){
             stage('Build'){}
-//        } else if(env.BRANCH_NAME ==~ "PR.*") {
-//            stage('Test Cases'){}
-//            stage('Integration Test Cases'){}
-//        } else if(env.TAG_NAME ==~ ".*") {
+        } else if(env.BRANCH_NAME ==~ "PR.*") {
+            stage('Test Cases'){}
+            stage('Integration Test Cases'){}
+        } else if(env.TAG_NAME ==~ ".*") {
             stage('Build') {}
             stage('Release App') {}
         }
