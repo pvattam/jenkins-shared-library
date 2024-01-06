9d25e59d332db7bfb328fc2992962cfb86fc8476
Pushpak Vattam <vattampushpak243@gmail.com>
2024-01-06 16:57:58 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -41,7 +41,7 @@ def call(){
             }
             stage('Code Quality'){
 
-                env.SONAR_TOKEN = AWS_SSM_PARAM(param_name: 'sonar.token')
+                env.SONAR_TOKEN = AWS_SSM_PARAM('sonar.token')
                 sh 'sonar-scanner -Dsonar.host.url=http://18.215.231.226:9000 -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectKey=expense-backend'
 
             }
