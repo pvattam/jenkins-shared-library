ba32e23496cc53b82d6095e67019f1dd16349e8b
Pushpak Vattam <vattampushpak243@gmail.com>
2024-01-10 22:26:07 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -6,26 +6,7 @@ def AWS_SSM_PARAM(param_name) {
 def call(){
     node ('workstation'){
 
-        sh "find . | sed -e '1d' |xargs rm -rf"
-
-        if(env.TAG_NAME ==~ ".*") {
-            env.branch_name = "refs/tags/${env.TAG_NAME}"
-        }else {
-            if (env.BRANCH_NAME ==~ "PR-.*"){
-                env.branch_name = "${env.CHANGE_BRANCH}"
-            }else{
-                env.BRANCH_NAME = "${env.BRANCH_NAME}"
-            }
-
-        }
-
-        stage('Code Checkout'){
-            checkout scmGit(
-                    branches: [[name: "${branch_name}"]],
-                    userRemoteConfigs: [[url: "https://github.com/pvattam/${repo_name}.git"]]
-            )
-            sh 'cat Jenkinsfile'
-        }
+        common.codeCheckout()
 
         if(app_type == "nodejs"){
             stage('Download Dependencies'){
