cfe331b628a59bd5d59bea3e6dec5b60c7131b3a
Pushpak Vattam <vattampushpak243@gmail.com>
2024-01-21 19:21:24 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- vars/goldenAmi.groovy
+++ vars/goldenAmi.groovy
@@ -3,16 +3,16 @@ def call() {
         node('workstation') {
             common.codeCheckout()
 
-            stage('Terraform Apply') {
-                sh 'terraform init'
-                sh 'terraform apply -auto-approve'
-            }
-
-            stage('Terroform Destroy'){
-                sh 'terraform init'
-                sh 'terraform state rm aws_ami_from_instance.ami'
-                sh 'terraform destroy -auto-approve'
-            }
+//            stage('Terraform Apply') {
+//                sh 'terraform init'
+//                sh 'terraform apply -auto-approve'
+//            }
+//
+//            stage('Terroform Destroy'){
+//                sh 'terraform init'
+//                sh 'terraform state rm aws_ami_from_instance.ami'
+//                sh 'terraform destroy -auto-approve'
+//            }
         }
     }
 }
